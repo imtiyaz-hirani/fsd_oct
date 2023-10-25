c349d05069a22d686fc5c24c1607f6ce2528e003
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-10-25 12:57:40 +0530

    Logistics-CS: DB connection + CS Statement

--- core java programming-refresher/CaseStudy4-LogisticsSystem/src/com/controller/LogisticsController.java
+++ core java programming-refresher/CaseStudy4-LogisticsSystem/src/com/controller/LogisticsController.java
@@ -1,5 +1,23 @@
 package com.controller;
 
 public class LogisticsController {
+	public static void main(String[] args) {
+		System.out.println("Enter the Source City(Pick Up City):");
+		//read source
+		/* Step 1: Go to DB and fetch the list of all centerMaps for the given source 
+		 * List<CenterMap> 
+		 * 
+		 *  1 Mumbai   	hyderabad     1200		3	 
+			2 Mumbai   	chennai       1400		3
+			3 Mumbai   	pune	       300		1
 
+		 * Select ID for costing
+		 * 1 
+		 * Number Of Days: 3
+		 * Cost for Delivery: INR 24 
+		 * */
+	}
 }
+/* ctrl + Shft + F 
+ * ctrl + space to auto complete the code 
+ */
