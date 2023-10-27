56af9f9887e4f1bc2eea0dcc6ae708b1c62d184f
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-10-27 10:30:04 +0530

    EmployeeRewardSystem-Models added

---
+++ core java programming-refresher/CaseStudy6-Hibernate-FinancialSystem/src/com/model/User.java
@@ -0,0 +1,60 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.enums.Role;
+
+@Entity
+@Table(name = "user_info")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "userid", nullable = false)
+	private String username;
+	private String password;
+	private Role role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
+
+}
