5fd009beccace97fd32f974bdc1a4df695b8c3e7
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-10-30 15:53:49 +0530

    Reading input from JSP in Servlet/controller

--- core java programming-refresher/CaseStudy7-SpringMVC-BusRes/src/com/spring/main/controller/SearchController.java
+++ core java programming-refresher/CaseStudy7-SpringMVC-BusRes/src/com/spring/main/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.spring.main.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SearchController {
 
 	@RequestMapping("/search")
-	public String readSearchInput() {
+	public String readSearchInput(HttpServletRequest request) { //DI: Dependency Injection
 		/* Read search parameters coming from JSP */
+		String source =request.getParameter("source");
+		String destination =request.getParameter("destination");
+		String doj =request.getParameter("doj");
 		
+		System.out.println(source + "---" + destination + "---" + doj);
 		/* Give these inputs to Service class and fetch the busses matching the inputs */
 		
 		/* Give the result as a List to searchResult jsp for display */
