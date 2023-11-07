7e0cbe25a243da2f4729fc097bfeb1960f45e754
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-11-07 09:51:36 +0530

    jiraclone-model classes added

---
+++ spring boot projects/SpringBootAppJiraClone/src/main/java/com/springboot/main/jira/model/Project.java
@@ -0,0 +1,45 @@
+package com.springboot.main.jira.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id; 
+	
+	private String title; 
+	
+	private String shortDesc;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getShortDesc() {
+		return shortDesc;
+	}
+
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	} 
+	
+	
+}
