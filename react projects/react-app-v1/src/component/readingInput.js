ad3209012ef9ca0fd07b03e8ed06c85964d700ad
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-11-15 10:21:24 +0530

    sign up component with level 1 validation

--- react projects/react-app-v1/src/component/readingInput.js
+++ react projects/react-app-v1/src/component/readingInput.js
@@ -2,23 +2,76 @@ import { useState } from "react";
 
 function ReadingInput(){
     let [name,setName] = useState('');
+    let [email,setEmail] = useState('');
+    let [password,setPassword] = useState('');
 
-    const process=()=>{
-        console.log(name);
+    let [nameMsg,setNameMsg] = useState('');
+    let [emailMsg,setEmailMsg] = useState('');
+    let [passwordMsg,setPasswordMsg] = useState('');
+    
+    const process=()=>{   //REGEX
+        console.log(name + '--' + email + '--' + password);
+        let user={
+            'name': name,
+            'email': email,
+            'password': password
+        }
+        let status = validate(user);
+
+        if(status === true){
+            console.log('sign up success, call API ')
+        }
+        else{
+            console.log('sign up unsuccessful, wait ')
+        }
     }
 
-    const changeHandler =(event)=>{
-        console.log(event);
+    const validate = (user)=>{
+        if(user.name === ''){
+            setNameMsg('name is required')
+            return false
+        }else{
+            setNameMsg('')
+        }
+
+        if(user.email === ''){
+            setEmailMsg('email is required')
+            return false
+        }
+        else{
+            setEmailMsg('')
+        }
+
+        if(user.password === ''){
+            setPasswordMsg('password is required')
+            return false
+        }
+        else{
+            setPasswordMsg('')
+        }
+
+        return true
     }
+
     return(
         <div>
             <h3>Reading input from User: </h3>
 
             <div>
                 <label>Please enter your name: </label>
-                <input type="text" value={name} onChange={(e)=>changeHandler(e.target.value) }/>
+                <input type="text" onChange={(e)=>setName(e.target.value) }/>
+                <span>{nameMsg}</span>
                 <br />
-                <button onClick={()=>process()}>Submit</button>
+                <label>Please enter your email: </label>
+                <input type="text" onChange={(e)=>setEmail(e.target.value) }/>
+                <span>{emailMsg}</span>
+                <br />
+                <label>Please enter your Password: </label>
+                <input type="text" onChange={(e)=>setPassword(e.target.value) }/>
+                <span>{passwordMsg}</span>
+                <br />
+                
+                <button onClick={()=>process()}>Sign Up</button>
             </div>
         </div>
     )
